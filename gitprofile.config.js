bbf60414a46c5a4727a2d7d2f43a239790bfc266
GowthamGottimukkala <gowtham.gottimukkala@gmail.com>
2024-02-05 18:27:00 -0500

    adds avatar url

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -27,7 +27,7 @@ const config = {
     // website: 'https://gowthamgottimukkala.github.io/',
     // phone: '3527099179',
     email: 'v.gottimukkala@ufl.edu',
-    avatarUrl: 'https://drive.google.com/uc?export=view&id=1VjP140_S9jTmu-j8dg0qVHBL5T3Prka1',
+    avatarUrl: 'https://drive.usercontent.google.com/download?id=1VjP140_S9jTmu-j8dg0qVHBL5T3Prka1&export=view&authuser=0',
   },
   resume: {
     fileUrl:
